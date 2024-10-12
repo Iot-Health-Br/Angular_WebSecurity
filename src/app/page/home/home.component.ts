81a2d34236fbd1cd93564060dc6c8be349296402
Iot-Health-Br <igl_macedo@hotmail.com>
2024-10-12 12:05:19 -0300

    V.1.1 - Feita a inclusão das importações do NG prime no angular.json, melhorado os templates das telas home e login.

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
+import {Button, ButtonModule} from "primeng/button";
+import {InputTextModule} from "primeng/inputtext";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ToastModule} from "primeng/toast";
+import {CardModule} from "primeng/card";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    Button,
+    InputTextModule,
+    ReactiveFormsModule,
+    ToastModule,
+    FormsModule,
+    CardModule,
+    ButtonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -2,24 +2,44 @@ import { Component } from '@angular/core';
 import {NgIf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {AuthServiceService} from "../../service/auth-service.service";
+import {CardModule} from "primeng/card";
+import {InputTextModule} from "primeng/inputtext";
+import {FloatLabelModule} from "primeng/floatlabel";
+import {PasswordModule} from "primeng/password";
+import {ToastModule} from "primeng/toast";
+import {Button, ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
+import {MessagesModule} from "primeng/messages";
+import {MessageService} from "primeng/api";
+import {ButtonGroupModule} from "primeng/buttongroup";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     NgIf,
-    FormsModule
+    FormsModule,
+    CardModule,
+    InputTextModule,
+    MessagesModule,
+    FloatLabelModule,
+    PasswordModule,
+    ToastModule,
+    Button,
+    ButtonModule,
+    RippleModule,
+    ButtonGroupModule,
   ],
+  providers: [MessageService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
   username: string = '';
   password: string = '';
   errorMessage: string = '';
 
-  constructor(private authService: AuthServiceService) {}
+  constructor(private authService: AuthServiceService, private messageService: MessageService) {}
 
   onLogin(): void {
     const success = this.authService.login(this.username, this.password);
